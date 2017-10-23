0194e598e42d2028401cd49157ebace85de0ec7b
batmansfriend <spk261096@gmail.com>
2017-10-23 11:19:20 +0530

    just for testing

--- app/src/main/java/com/pkstudios/dev/fansapp/MainActivity.java
+++ app/src/main/java/com/pkstudios/dev/fansapp/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//just for testing
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
