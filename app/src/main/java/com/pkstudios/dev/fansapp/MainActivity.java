4c41a1abfe250f6cdbffd636e26bd2181c9ff947
prasanna189 <spkforwork@gmail.com>
2017-10-23 11:21:17 +0530

    another test commit

--- app/src/main/java/com/pkstudios/dev/fansapp/MainActivity.java
+++ app/src/main/java/com/pkstudios/dev/fansapp/MainActivity.java
@@ -13,7 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-//just for testing
+//just testing
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
